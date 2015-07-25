44606b8af9b74a5229686467f22d534ee3b0105a
jzahka <john.zahka@gmail.com>
2015-07-25 15:47:19 -0400

    setup react prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # react addons
+  config.react.addons = true # defaults to false
+  config.react.variant = :production
 end
